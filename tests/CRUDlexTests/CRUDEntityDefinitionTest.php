a44be9f0cb41dfc7594e03ff158f63d5982d1b7a
philiplb <philip@philiplb.de>
2014-08-31 18:02:15 +0200

    testing the rest of CRUDEntityDefinition

--- tests/CRUDlexTests/CRUDEntityDefinitionTest.php
+++ tests/CRUDlexTests/CRUDEntityDefinitionTest.php
@@ -19,6 +19,8 @@ class CRUDEntityDefinitionTest extends \PHPUnit_Framework_TestCase {
 
     protected $definition;
 
+    protected $definitionLibrary;
+
     protected function setUp() {
         $crudServiceProvider = new CRUDServiceProvider();
         $dataFactory = new CRUDTestDataFactory();
@@ -26,6 +28,7 @@ class CRUDEntityDefinitionTest extends \PHPUnit_Framework_TestCase {
         $stringsFile = __DIR__.'/../../src/strings.yml';
         $crudServiceProvider->init($dataFactory, $crudFile, $stringsFile);
         $this->definition = $crudServiceProvider->getData('book')->getDefinition();
+        $this->definitionLibrary = $crudServiceProvider->getData('library')->getDefinition();
     }
 
     public function testGetFieldNames() {
@@ -67,6 +70,99 @@ class CRUDEntityDefinitionTest extends \PHPUnit_Framework_TestCase {
             'pages',
             'release',
             'library'
+        );
+        $this->assertSame($read, $expected);
+    }
+
+    public function testIsRequired() {
+        $read = $this->definition->isRequired('title');
+        $this->assertTrue($read);
+        $read = $this->definition->isRequired('release');
+        $this->assertFalse($read);
+        $read = $this->definition->isRequired('false');
+        $this->assertFalse($read);
+        $read = $this->definition->isRequired(null);
+        $this->assertFalse($read);
+    }
+
+    public function testGetReferenceTable() {
+        $read = $this->definition->getReferenceTable('library');
+        $expected = 'library';
+        $this->assertSame($read, $expected);
+        $read = $this->definition->getReferenceTable('title');
+        $this->assertNull($read);
+        $read = $this->definition->getReferenceTable('foo');
+        $this->assertNull($read);
+        $read = $this->definition->getReferenceTable(null);
+        $this->assertNull($read);
+    }
+
+    public function testGetReferenceNameField() {
+        $read = $this->definition->getReferenceNameField('library');
+        $expected = 'name';
+        $this->assertSame($read, $expected);
+        $read = $this->definition->getReferenceNameField('title');
+        $this->assertNull($read);
+        $read = $this->definition->getReferenceNameField('foo');
+        $this->assertNull($read);
+        $read = $this->definition->getReferenceNameField(null);
+        $this->assertNull($read);
+    }
+
+    public function testGetReferenceEntity() {
+        $read = $this->definition->getReferenceEntity('library');
+        $expected = 'library';
+        $this->assertSame($read, $expected);
+        $read = $this->definition->getReferenceEntity('title');
+        $this->assertNull($read);
+        $read = $this->definition->getReferenceEntity('foo');
+        $this->assertNull($read);
+        $read = $this->definition->getReferenceEntity(null);
+        $this->assertNull($read);
+    }
+
+    public function testIsUnique() {
+        $read = $this->definitionLibrary->isUnique('name');
+        $this->assertTrue($read);
+        $read = $this->definition->isUnique('release');
+        $this->assertFalse($read);
+        $read = $this->definition->isUnique('false');
+        $this->assertFalse($read);
+        $read = $this->definition->isUnique(null);
+        $this->assertFalse($read);
+    }
+
+    public function testGetFieldLabel() {
+        $read = $this->definition->getFieldLabel('library');
+        $expected = 'Library';
+        $this->assertSame($read, $expected);
+        $read = $this->definition->getFieldLabel('id');
+        $expected = 'Id';
+        $this->assertSame($read, $expected);
+        $read = $this->definition->getFieldLabel('foo');
+        $expected = 'foo';
+        $this->assertSame($read, $expected);
+        $read = $this->definition->getFieldLabel(null);
+        $this->assertNull($read);
+    }
+
+    public function testGetTable() {
+        $read = $this->definition->getTable();
+        $expected = 'book';
+        $this->assertSame($read, $expected);
+    }
+
+    public function testGetLabel() {
+        $read = $this->definition->getLabel();
+        $expected = 'Book';
+        $this->assertSame($read, $expected);
+    }
+
+    public function testAddAndGetParent() {
+        $this->definition->addParent('foo', 'bar');
+        $read = $this->definition->getParents();
+        $expected = array(
+            array('foo', 'bar')
         );
         $this->assertSame($read, $expected);
     }
