c6a58b5b18fe602c3972c07e9ec660e7700bed15
philiplb <philip@philiplb.de>
2014-09-23 00:13:34 +0200

    testing CRUDData::createFiles()

--- tests/CRUDlexTestEnv/CRUDNullFileProcessor.php
+++ tests/CRUDlexTestEnv/CRUDNullFileProcessor.php
@@ -17,15 +17,54 @@ use Symfony\Component\HttpFoundation\Request;
 
 class CRUDNullFileProcessor implements CRUDFileProcessorInterface {
 
+    private $createFileCalled;
+
+    private $updateFileCalled;
+
+    private $deleteFileCalled;
+
+    private $renderFileCalled;
+
+    public function __construct() {
+        $this->reset();
+    }
+
+    public function reset() {
+        $this->createFileCalled = false;
+        $this->updateFileCalled = false;
+        $this->deleteFileCalled = false;
+        $this->renderFileCalled = false;
+    }
+
     public function createFile(Request $request, CRUDEntity $entity, $entityName, $field) {
+        $this->createFileCalled = true;
     }
 
     public function updateFile(Request $request, CRUDEntity $entity, $entityName, $field) {
+        $this->updateFileCalled = true;
     }
 
     public function deleteFile(CRUDEntity $entity, $entityName, $field) {
+        $this->deleteFileCalled = true;
     }
 
     public function renderFile(CRUDEntity $entity, $entityName, $field) {
+        $this->renderFileCalled = true;
+    }
+
+    public function isCreateFileCalled() {
+        return $this->createFileCalled;
+    }
+
+    public function isUpdateFileCalled() {
+        return $this->updateFileCalled;
+    }
+
+    public function isDeleteFileCalled() {
+        return $this->deleteFileCalled;
+    }
+
+    public function isRenderFileCalled() {
+        return $this->renderFileCalled;
     }
 }
