02638bf66da255d282f87fa926cd2a2ae6de8f4c
philiplb <philip@philiplb.de>
2014-09-27 01:00:47 +0200

    (yet empty) tests for the simplefilesystemfileprocessor

---
+++ tests/CRUDlexTests/CRUDSimpleFilesystemFileProcessorTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the CRUDlex package.
+ *
+ * (c) Philip Lehmann-Böhm <philip@philiplb.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CRUDlexTests;
+
+class CRUDSimpleFilesystemFileProcessorTest extends \PHPUnit_Framework_TestCase {
+
+
+
+}
