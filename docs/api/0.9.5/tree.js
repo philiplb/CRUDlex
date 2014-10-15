1e471032fa06320d9be3b529c061b0e6176d456a
philiplb <philip@philiplb.de>
2014-10-15 21:32:47 +0200

    initial api docs generation

---
+++ docs/api/0.9.5/tree.js
@@ -0,0 +1,13 @@
+var tree = [["CRUDlex","CRUDlex.html","",
+                [["CRUDControllerProvider","CRUDlex\/CRUDControllerProvider.html","",
+                [        ]],["CRUDData","CRUDlex\/CRUDData.html","",
+                [        ]],["CRUDDataFactoryInterface","CRUDlex\/CRUDDataFactoryInterface.html","",
+                [        ]],["CRUDEntity","CRUDlex\/CRUDEntity.html","",
+                [        ]],["CRUDEntityDefinition","CRUDlex\/CRUDEntityDefinition.html","",
+                [        ]],["CRUDFileProcessorInterface","CRUDlex\/CRUDFileProcessorInterface.html","",
+                [        ]],["CRUDMySQLData","CRUDlex\/CRUDMySQLData.html"," < CRUDData",
+                [        ]],["CRUDMySQLDataFactory","CRUDlex\/CRUDMySQLDataFactory.html","",
+                [        ]],["CRUDServiceProvider","CRUDlex\/CRUDServiceProvider.html","",
+                [        ]],["CRUDSimpleFilesystemFileProcessor","CRUDlex\/CRUDSimpleFilesystemFileProcessor.html","",
+                [        ]]        ]]        ]
+
