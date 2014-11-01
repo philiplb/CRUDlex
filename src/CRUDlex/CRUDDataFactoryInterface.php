4f9f1f74de61601a90da13cc81cd1d26d87ef728
philiplb <philip@philiplb.de>
2014-11-01 10:37:35 +0100

    API docs for CRUDDataFactoryInterface

--- src/CRUDlex/CRUDDataFactoryInterface.php
+++ src/CRUDlex/CRUDDataFactoryInterface.php
@@ -15,8 +15,25 @@ use CRUDlex\CRUDData;
 use CRUDlex\CRUDEntityDefinition;
 use CRUDlex\CRUDFileProcessorInterface;
 
+/**
+ * An interface used by the {@see CRUDServiceProvider} to construct
+ * {@see CRUDData} instances. By implementing this and handing it into
+ * the service provider, the user can control what database (-variation) he
+ * wants to use.
+ */
 interface CRUDDataFactoryInterface {
 
+    /**
+     * Creates instances.
+     *
+     * @param CRUDEntityDefinition $definition
+     * the definition of the entities managed by the to be created instance
+     * @param CRUDFileProcessorInterface $fileProcessor
+     * the file processor managing uploaded files
+     *
+     * @return CRUDData
+     * the newly created instance
+     */
     public function createData(CRUDEntityDefinition $definition, CRUDFileProcessorInterface $fileProcessor);
 
 }
