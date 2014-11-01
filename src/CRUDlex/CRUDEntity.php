1ce0205db61d45eac7f48dfe3f3eeca213cb20e0
philiplb <philip@philiplb.de>
2014-11-01 11:17:30 +0100

    API docs of CRUDEntity

--- src/CRUDlex/CRUDEntity.php
+++ src/CRUDlex/CRUDEntity.php
@@ -15,20 +15,56 @@ use CRUDlex\CRUDEntityDefinition;
 
 use CRUDlex\CRUDData;
 
+/**
+ * Represents a single set of data in field value pairs like the row in a
+ * database. Depends of course on the {@see CRUDData} implementation being used.
+ * With this objects, the data is passed arround and validated.
+ */
 class CRUDEntity {
 
+    /**
+     * The {@see CRUDEntityDefinition} defining how this entity looks like.
+     */
     protected $definition;
 
+    /**
+     * Holds the key value data of the entity.
+     */
     protected $entity = array();
 
+    /**
+     * Constructor.
+     *
+     * @param CRUDEntityDefinition $definition
+     * the definition how this entity looks
+     */
     public function __construct(CRUDEntityDefinition $definition) {
         $this->definition = $definition;
     }
 
+    /**
+     * Sets a field value pair of this entity.
+     *
+     * @param string $field
+     * the field
+     * @param mixed $value
+     * the value
+     */
     public function set($field, $value) {
         $this->entity[$field] = $value;
     }
 
+    /**
+     * Gets the value of a field.
+     *
+     * @param string $field
+     * the field
+     *
+     * @return mixed
+     * null on invalid field, an int if the definition says that the
+     * type of the field is an int, a boolean if the field is a bool or
+     * else the raw value
+     */
     public function get($field) {
         if (!key_exists($field, $this->entity)) {
             return null;
@@ -45,10 +81,33 @@ class CRUDEntity {
         return $value;
     }
 
+    /**
+     * Gets the entity definition.
+     *
+     * @return CRUDEntityDefinition
+     * the definition
+     */
     public function getDefinition() {
         return $this->definition;
     }
 
+    /**
+     * Validates the entity against the definition.
+     *
+     * @param CRUDData $data
+     * the data access instance used for counting things
+     *
+     * @return array
+     * an array with the fields "valid" and "errors"; valid provides a quick
+     * check whether the given entity passes the validation and errors is an
+     * array with all fields as keys and arrays as values; this field arrays
+     * contain three keys: required, unique and input; each of them represents
+     * with a boolean whether the input is ok in that way; if "required" is
+     * true, the field wasn't set, unique means the uniqueness of the field in
+     * the datasource and input is used to indicate whether the form of the
+     * value is correct (a valid int, date, depending on the type in the
+     * definition)
+     */
     public function validate(CRUDData $data) {
 
         $fields = $this->definition->getEditableFieldNames();
