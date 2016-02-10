a84569597af1d166aeb94074cd37a61daa884fdf
philiplb <philip@philiplb.de>
2016-02-10 23:43:09 +0100

    generated 0.9.9 API docs

---
+++ docs/api/0.9.9/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","CRUDlex\\CRUDControllerProvider"],["c","CRUDlex\\CRUDData"],["c","CRUDlex\\CRUDDataFactoryInterface"],["c","CRUDlex\\CRUDEntity"],["c","CRUDlex\\CRUDEntityDefinition"],["c","CRUDlex\\CRUDEntityValidator"],["c","CRUDlex\\CRUDFileProcessorInterface"],["c","CRUDlex\\CRUDMySQLData"],["c","CRUDlex\\CRUDMySQLDataFactory"],["c","CRUDlex\\CRUDServiceProvider"],["c","CRUDlex\\CRUDSimpleFilesystemFileProcessor"],["c","CRUDlex\\CRUDStreamedFileResponse"]];
