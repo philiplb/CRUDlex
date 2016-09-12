6550ad5a0c861bac5fe51e81aa81ab7945307e4b
Philip Lehmann-Boehm <philip@philiplb.de>
2016-09-12 20:11:08 +0200

    testing Entity::get with reference and many types

--- tests/CRUDlexTests/EntityTest.php
+++ tests/CRUDlexTests/EntityTest.php
@@ -29,17 +29,22 @@ class EntityTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function testGetSet() {
+        $definitionLibrary = $this->crudServiceProvider->getData('library')->getDefinition();
+        $library = $this->crudServiceProvider->getData('library')->createEmpty();
+        $library->set('name', 'lib a');
+        $this->crudServiceProvider->getData('library')->create($library);
+
         $definition = $this->crudServiceProvider->getData('book')->getDefinition();
         $entity = new Entity($definition);
         $entity->set('test', 'testdata');
         $read = $entity->get('test');
         $expected = 'testdata';
-        $this->assertSame($expected, $read);
+        $this->assertSame($read, $expected);
 
         $entity->set('test', 'testdata2');
         $read = $entity->get('test');
         $expected = 'testdata2';
-        $this->assertSame($expected, $read);
+        $this->assertSame($read, $expected);
 
         $read = $entity->get('testNull');
         $this->assertNull($read);
@@ -47,38 +52,54 @@ class EntityTest extends \PHPUnit_Framework_TestCase {
         $entity->set('price', 3.99);
         $read = $entity->get('price');
         $expected = 3.99;
-        $this->assertSame($expected, $read);
+        $this->assertSame($read, $expected);
 
         $entity->set('pages', 111);
         $read = $entity->get('pages');
         $expected = 111;
-        $this->assertSame($expected, $read);
+        $this->assertSame($read, $expected);
+
+        $entity->set('library', $library->get('id'));
+        $read = $entity->get('library');
+        $expected = $library->get('id');
+        $this->assertSame($read, $expected);
+
+        $entity = $this->crudServiceProvider->getData('book')->createEmpty();
+        $entity->set('title', 'title a');
+        $entity->set('author', 'author a');
+        $entity->set('pages', 1);
+        $entity->set('library', $library->get('id'));
+        $entity->set('cover', 'cover a');
+        $this->crudServiceProvider->getData('book')->create($entity);
+
+        $library->set('libraryBook', [['id' => $entity->get('id')]]);
+        $read = $library->get('libraryBook');
+        $expected = [['id' => $entity->get('id')]];
+        $this->assertSame($read, $expected);
 
         // Fixed values override
         $definition->setValue('pages', 666);
         $entity->set('pages', 111);
         $read = $entity->get('pages');
         $expected = 666;
-        $this->assertSame($expected, $read);
+        $this->assertSame($read, $expected);
 
-
-        $definition = $this->crudServiceProvider->getData('library')->getDefinition();
-        $entity = new Entity($definition);
+        $entity = new Entity($definitionLibrary);
 
         $entity->set('isOpenOnSundays', true);
         $read = $entity->get('isOpenOnSundays');
         $expected = true;
-        $this->assertSame($expected, $read);
+        $this->assertSame($read, $expected);
 
         $entity->set('opening', '');
         $read = $entity->get('opening');
         $expected = null;
-        $this->assertSame($expected, $read);
+        $this->assertSame($read, $expected);
 
         $entity->set('opening', '2016-09-12 01:02:03');
         $read = $entity->get('opening');
         $expected = '2016-09-12 01:02:03';
-        $this->assertSame($expected, $read);
+        $this->assertSame($read, $expected);
 
     }
 
