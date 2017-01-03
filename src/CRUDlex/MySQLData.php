83cad432b5b41e1675210e821e7209738f55d8c9
Philip Lehmann-Boehm <philip@philiplb.de>
2017-01-03 22:46:35 +0100

    possible simplification

--- src/CRUDlex/MySQLData.php
+++ src/CRUDlex/MySQLData.php
@@ -303,18 +303,21 @@ class MySQLData extends AbstractData {
 
         $queryBuilder->setParameter(0, $ids, Connection::PARAM_STR_ARRAY);
 
-        $queryResult = $queryBuilder->execute();
-        $rows        = $queryResult->fetchAll(\PDO::FETCH_ASSOC);
-        $amount      = count($entities);
+        $queryResult          = $queryBuilder->execute();
+        $rows                 = $queryResult->fetchAll(\PDO::FETCH_ASSOC);
+        $amount               = count($entities);
+        $enrichReferenceField = function($i, array $row) use ($field, $entities, $nameField) {
+            if ($entities[$i]->get($field) == $row['id']) {
+                $value = ['id' => $entities[$i]->get($field)];
+                if ($nameField) {
+                    $value['name'] = $row[$nameField];
+                }
+                $entities[$i]->set($field, $value);
+            }
+        };
         foreach ($rows as $row) {
             for ($i = 0; $i < $amount; ++$i) {
-                if ($entities[$i]->get($field) == $row['id']) {
-                    $value = ['id' => $entities[$i]->get($field)];
-                    if ($nameField) {
-                        $value['name'] = $row[$nameField];
-                    }
-                    $entities[$i]->set($field, $value);
-                }
+                $enrichReferenceField($i, $row);
             }
         }
     }
