2f76d28e919958be42f10b27f793c59eb6ba5d14
Philip Lehmann-Boehm <philip@philiplb.de>
2017-03-22 23:08:43 +0100

    updated the copyright year of the docs

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'CRUDlex'
-copyright = u'2016, Philip Lehmann-Böhm'
+copyright = u'2017, Philip Lehmann-Böhm'
 author = u'Philip Lehmann-Böhm'
 
 # The version info for the project you're documenting, acts as replacement for
